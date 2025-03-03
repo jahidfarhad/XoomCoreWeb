14bc182ae5f2bed2354d7828c5fc516a26dbbf27
Choton Mohammad Ishaque <ChotonMohammed@gmail.com>
2025-03-04 00:49:22 +0600

    Add employee request feature and update connection string
    Updated database.json to change the server name in the connection string to XOOMSTATION.
    
    In HomeController.cs:
    - Commented out MustHaveAuthorize and MustHavePermission attributes.
    - Added static list createEmployeeRequests.
    - Added GetUser, GetEmployeeList, and CreateEmployeeRequest action methods.
    
    In Index.cshtml:
    - Made form submission handler for #newForm asynchronous.
    - Constructed createRequest object from form inputs and made async POST request to /CreateEmployeeRequest endpoint.
    
    Added CreateEmployeeRequest.cs to define CreateEmployeeRequest class with properties for FirstName, LastName, BirthDate, Email, Phone, and City.

---
+++ XoomCore.Application/RequestModels/CreateEmployeeRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace XoomCore.Application.RequestModels;
+
+public class CreateEmployeeRequest
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string BirthDate { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string City { get; set; }
+}
--- XoomCore.Web/Controllers/HomeController.cs
+++ XoomCore.Web/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using XoomCore.Application.RequestModels;
+using XoomCore.Application.ResponseModels.Shared;
 using XoomCore.Services.SessionControl;
 
 namespace XoomCore.Web.Controllers;
 
-[MustHaveAuthorize]
+//[MustHaveAuthorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly ISessionManager _sessionManager;
+    public static List<CreateEmployeeRequest> createEmployeeRequests = new List<CreateEmployeeRequest>();
     public HomeController(ILogger<HomeController> logger, ISessionManager sessionManager)
     {
         _logger = logger;
@@ -15,16 +18,35 @@ public class HomeController : Controller
 
     }
     [Route("index")]
-    [MustHavePermission(SubMenuKey: "Home", Controller: "Home", Action: "Index")]
+    //[MustHavePermission(SubMenuKey: "Home", Controller: "Home", Action: "Index")]
     public IActionResult Index()
     {
         return View();
     }
+
     [HttpGet]
     [Route("GetUser")]
     public ActionResult GetUser()
     {
         return Ok(new { error = false, MenuList = _sessionManager.Current.SubMenuAuthorizedList, UserId = _sessionManager.Current.UserId });
     }
-
+    [Route("GetEmployeeList")]
+    [HttpPost]
+    public async Task<CommonResponse<List<CreateEmployeeRequest>>> GetEmployeeList()
+    {
+        return CommonResponse<List<CreateEmployeeRequest>>.CreateHappyResponse(createEmployeeRequests);
+    }
+    [Route("CreateEmployeeRequest")]
+    [HttpPost]
+    public async Task<CommonResponse<string>> CreateEmployeeRequest([FromBody] CreateEmployeeRequest request)
+    {
+        _logger.LogInformation("CreateEmployeeRequest by : {email}", request.Email);
+        if (!ModelState.IsValid)
+        {
+            string errorMessage = ModelState.Values.SelectMany(v => v.Errors).FirstOrDefault()?.ErrorMessage;
+            return CommonResponse<string>.CreateWarningResponse(message: errorMessage);
+        }
+        createEmployeeRequests.Add(request);
+        return CommonResponse<string>.CreateHappyResponse(message: "Success");
+    }
 }
